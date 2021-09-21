8cbbbacaf5c700f204f92bbaad98c910ceb1c39c
Breno Henrique <bhfs.2908@gmail.com>
2021-09-21 09:48:39 -0300

    feat:finish board

--- components/Houses.jsx
+++ components/Houses.jsx
@@ -1,5 +1,12 @@
 import styles from "../styles/Houses.module.css";
 
 export default function Houses(props) {
-  return <div className={styles.houses}></div>;
+  return (
+    <div
+      style={{
+        backgroundColor: props.preta ? "#000" : "#fff",
+      }}
+      className={styles.houses}
+    ></div>
+  );
 }
